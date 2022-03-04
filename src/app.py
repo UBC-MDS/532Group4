8e2fcd77c8b2c2ff52f21b44069fb4bd1556eb1c
Abhiket Gaurav <p12abhiketg@iimidr.ac.in>
2022-03-04 10:43:37 -0800

    updated_file

--- src/app.py
+++ src/app.py
@@ -4,8 +4,26 @@ import altair as alt
 import pandas as pd
 
 # disable Altair limits
-qwl_df = pd.read_csv("./data/bei_vita_qwl_assessment.csv")
+qwl_df = pd.read_csv("../data/bei_vita_qwl_assessment.csv")
 
+# NEW Data wrangling for vertical bar graph
+workshop_topics = ['Stress Optimization',
+              'Mindset Coaching',
+              'Sleep Strategies',
+              'Social Wellbeing',
+              'Leadership and Teamwork',
+              'Physical Health & Fitness',
+              'Nutrition & Gut Health']
+
+temp = qwl_df.iloc[:,55:62]
+temp = temp.set_axis(workshop_topics, axis=1)
+temp['Country'] = qwl_df.iloc[:,4]
+temp = temp.set_index('Country')
+
+vg_df = temp.melt(var_name = "Workshop_Topic", value_name = "Preference", ignore_index = False)
+vg_df = vg_df.reset_index(level=0)
+
+#Dash app
 app = Dash(
     __name__,
     external_stylesheets=[dbc.themes.BOOTSTRAP, "https://codepen.io/chriddyp/pen/bWLwgP.css"]
@@ -54,7 +72,8 @@ app.layout = dbc.Container(
                 dbc.Col(
                     [
                         html.Div(
-                            [ html.H6("Role Classification"),
+                            [
+                                html.H6("Role Classification"),
                                 dcc.Checklist(
                                     id = 'my_checklist_1',
                                     options = [{'label' : x , 'value' : x,'disabled':False}
@@ -80,7 +99,11 @@ app.layout = dbc.Container(
                 ),
                 dbc.Col(
                     [
-                   
+                    html.Iframe(
+                        id='horizontal_barplot',
+                        # srcDoc= plot_horizontal_barchart(),
+                        style={'border-width': '0', 'width': '100%', 'height': '400px'})  
+
                     ],
                     width=3
                 ),
@@ -98,16 +121,15 @@ app.layout = dbc.Container(
                 dbc.Col(
                     [
                         html.Div(
-                            [
-                                html.Iframe(
-                                    id="vertical_barplot",
-                                    # value="Total score",
-                                    style={"border-width": "0", "width": "100%", "height": "450px"}
-                                ),
+                            [ #Vertical boxplot drop down
+                                 html.Iframe(
+                                     id='Country',
+                                     style={'border-width': '0', 'width': '100%', 'height': '400px'}
+                                 ),
                                 dcc.Dropdown(
-                                    id='xcol-vbarplot-widget',
-                                    value='Total score',
-                                    options=[{'label': col, 'value': col} for col in ["Total score"]]
+                                    id='xcountry-widget',
+                                    value=vg_df.Country[0],  # REQUIRED for page to load with a graph
+                                    options=[{'label': c, 'value': c} for c in vg_df.Country.unique()]
                                 )
                             ]
                         )
@@ -119,14 +141,16 @@ app.layout = dbc.Container(
 )
 
 @app.callback(
-    Output("lineplot","srcDoc"),
-    Output("vertical_barplot", "srcDoc"),
+    Output("lineplot", "srcDoc"),
+    Output("horizontal_barplot", "srcDoc"),
+    Output('Country', 'srcDoc'),
     Input('my_checklist_1', 'value'),
     Input('my_checklist_2', 'value'),
-    Input('xcol-vbarplot-widget', 'value')
+    #Input('xcol-lineplot-widget', 'value'),
+    Input('xcountry-widget', 'value')
 )
-
 def vertical_barplot(my_checklist_1, my_checklist_2, xcol_vbarplot):
+    # line plot 
     df_sub = qwl_df[(qwl_df["Role Classification"].isin(my_checklist_1)) & 
                     (qwl_df["Country of Residence "].isin(my_checklist_2)) ]
     lineplot = alt.Chart(df_sub, title='How healthy are the employees feeling overall?').mark_area(
@@ -139,15 +163,28 @@ def vertical_barplot(my_checklist_1, my_checklist_2, xcol_vbarplot):
               title="# of Employees",
     )).properties(width=600).interactive()
 
-    vertical_barplot = alt.Chart(qwl_df).mark_bar().encode(
-        x=alt.X(
-            xcol_vbarplot,
-            bin=alt.Bin(maxbins=5)
-        ),
-        y="count()",
+    #vertical boxplot
+    data = vg_df[(vg_df['Country'] == xcol_vbarplot)]
+    vertical_boxplot = alt.Chart(data, title = "Workshop Preference (lower is more important)").mark_boxplot().encode(
+        x = 'Workshop_Topic',
+        y = 'Preference',
+        color = alt.Color('Workshop_Topic', legend=None)
+    ).properties(
+        width=400,
+        height=180
     )
+    
+    col_name = '17. I experience MEANINGFULNESS at work ... (e.g. inspired, trusted, respected, purpose, seen and heard, acknowledged, fulfilled, growth, contribution to something greater, etc.) '
 
-    return lineplot.to_html(), vertical_barplot.to_html(), 
+    plot_data = qwl_df[col_name].value_counts().to_frame().reset_index()
+    plot_data = plot_data.rename(columns={col_name:'Count', 'index':'Response'})
+
+    chart = alt.Chart(plot_data).mark_bar().encode(
+                    x='Count:Q',
+                    y='Response:O',  
+                )
+
+    return lineplot.to_html(), vertical_boxplot.to_html(), chart.to_html()
 
 if __name__ == "__main__":
     app.run_server(debug=False)
