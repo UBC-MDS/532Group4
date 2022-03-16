f82c7aced18e5ab11fb17d39d9ebc5e8784be833
Abhiket Gaurav <p12abhiketg@iimidr.ac.in>
2022-03-15 22:55:25 -0700

    Incorporated all the changes(as per peer review)
    Change 1 : Added filter for employee types as asked. 
    Change 2: Accept the feedback , changed the graph to a histogram
    Change 3: Rather than adjusting the scale we have added color to a different kind of response, this created a good visual effect for the kind of responses being captured. The user will instantly know when the repose variable is unique or more than one. 
    Change 4: We have removed the second filter from the dashboard and now the whole dashboard will depend on the country selected and the level of the employee (mentioned in graph's subtitle).
    Change 5 : Added a default value to the plot
    Change 6 : Added imaginary client name

--- src/app.py
+++ src/app.py
@@ -4,24 +4,7 @@ import altair as alt
 import pandas as pd
 
 # disable Altair limits
-qwl_df = pd.read_csv("../data/bei_vita_qwl_assessment.csv")
-
-# NEW Data wrangling for vertical bar graph
-workshop_topics = ['Stress Optimization',
-              'Mindset Coaching',
-              'Sleep Strategies',
-              'Social Wellbeing',
-              'Leadership and Teamwork',
-              'Physical Health & Fitness',
-              'Nutrition & Gut Health']
-
-temp = qwl_df.iloc[:,55:62]
-temp = temp.set_axis(workshop_topics, axis=1)
-temp['Country'] = qwl_df.iloc[:,4]
-temp = temp.set_index('Country')
-
-vg_df = temp.melt(var_name = "Workshop_Topic", value_name = "Preference", ignore_index = False)
-vg_df = vg_df.reset_index(level=0)
+qwl_df = pd.read_csv("data/bei_vita_qwl_assessment.csv")
 
 #Dash app
 app = Dash(
@@ -37,7 +20,7 @@ app.layout = dbc.Container(
                 dbc.Col(
                     [
                         html.Br(),
-                        html.H2("Bei Vita"),
+                        html.H3("Bei Vita"),
                         html.P(
                             dcc.Markdown(
                                 """
@@ -51,8 +34,8 @@ app.layout = dbc.Container(
                 ),
                 dbc.Col(
                     [
-                        html.H1("Quality of Work Life"),
-                        html.H2("Client Name")
+                        html.H2("Quality of Work Life"),
+                        html.H3("Client Name: HDBC Bank")
                     ],
                     width=6
                 ),
@@ -67,115 +50,130 @@ app.layout = dbc.Container(
         html.Br(),
 
         dbc.Row(
-            [
-                
+            [ 
                 dbc.Col(
                     [
                         html.Div(
                             [  
                                 html.H6("Country of Residence"),
                                 dcc.Dropdown(
-                                    id = 'my_checklist',
+                                    id = 'my_checklist_1',
                                     options = [{'label' : y , 'value' : y,'disabled':False}
                                     for y in qwl_df['Country of Residence '].unique()],
-                                    value = [c for c in sorted(qwl_df['Country of Residence '].unique())]),
+                                    value = 'HK & Macau'),
+
+                                html.H6("Role Classification"),
+                                dcc.Dropdown(
+                                    id = 'my_checklist_2',
+                                    options = [{'label' : x , 'value' : x,'disabled':False}
+                                    for x in qwl_df['Role Classification'].unique()],
+                                    value = 'Entry (Associate)'),
+
                                 html.Iframe(
-                                    id="lineplot",
-                                    style={"border-width": "0", "width": "100%", "height": "450px"}
+                                    id="histogram",
+                                    style={"border-width": "0", "width": "100%", "height": "340px"}
                                 )
                             ]
                         )
                     ],
-                    width=9
-                ),
-                dbc.Col(
-                    [
-                    html.Iframe(
-                        id='horizontal_barplot',
-                        # srcDoc= plot_horizontal_barchart(),
-                        style={'border-width': '0', 'width': '100%', 'height': '400px'})  
-
-                    ],
-                    width=3
-                ),
+                    width=12
+                )
             ],
         ),
-        html.Br(),
 
         dbc.Row(
             [
                 dbc.Col(
                     [
-                        
-                    ],
+                        html.Iframe(
+                            id='horizontal_barplot',
+                            style={'border-width': '0', 'width': '100%', 'height': '450px'})  
+                        ],
+                    width=6
                 ),
                 dbc.Col(
                     [
                         html.Div(
-                            [ #Vertical boxplot drop down
-                                 html.Iframe(
-                                     id='Country',
-                                     style={'border-width': '0', 'width': '100%', 'height': '400px'}
-                                 ),
-                                dcc.Dropdown(
-                                    id='xcountry-widget',
-                                    value=vg_df.Country[0],  # REQUIRED for page to load with a graph
-                                    options=[{'label': c, 'value': c} for c in vg_df.Country.unique()]
-                                )
+                            [
+                            html.Iframe(
+                                id='Country',
+                                style={'border-width': '0', 'width': '100%', 'height': '450px'})
                             ]
-                        )
-                    ]
-                ),
+                        ),
+                    ],
+                )
             ],
-        )
+        )   
     ]
 )
 
 @app.callback(
-    Output("lineplot", "srcDoc"),
+    Output('histogram', 'srcDoc'),
     Output("horizontal_barplot", "srcDoc"),
     Output('Country', 'srcDoc'),
-    Input('my_checklist', 'value'),
-    #Input('xcol-lineplot-widget', 'value'),
-    #Input('xcountry_2-widget', 'value'),
-    Input('xcountry-widget', 'value')
+    Input('my_checklist_1', 'value'),
+    Input('my_checklist_2', 'value')
 )
-def vertical_barplot(my_checklist, xcol_vbarplot):
 
-    df_sub = qwl_df[(qwl_df['Country of Residence '].isin([my_checklist]))]
-  
-    lineplot = alt.Chart(df_sub, title='How healthy are the employees feeling overall?').mark_area(
-        color = "lightblue",
-        interpolate = "step-after",
-        line = True
-    ).encode(
-        alt.X("Total score"),
+def vertical_barplot(my_checklist_1,my_checklist_2):
+    plot_data = qwl_df[(qwl_df["Country of Residence "].isin([my_checklist_1])) & 
+                        (qwl_df["Role Classification"].isin([my_checklist_2]))]
+
+    #histogram
+    histogram = alt.Chart(plot_data, ).mark_bar(
+        color = "lightblue").encode(
+        alt.X("Total score", bin = True),
         alt.Y("count():Q",
-              title="# of Employees",
-    )).properties(width=600).interactive()
+              title="# of Employees", axis = alt.Axis(values = list(range(1,17,1)))
+    )).properties(width=750, height=250, title= {'text': ['How healthy are the employees feeling overall?'],'subtitle': 'At country and designation level'}).interactive()
+
+    #horizontal barchart
+    col_name = '17. I experience MEANINGFULNESS at work ... (e.g. inspired, trusted, respected, purpose, seen and heard, acknowledged, fulfilled, growth, contribution to something greater, etc.) '
+
+    plot_data = plot_data[col_name].value_counts().to_frame().reset_index()
+    plot_data = plot_data.rename(columns={col_name:'Count', 'index':'Response'})
+
+    chart = alt.Chart(plot_data).mark_bar().encode(
+        x='Count:Q',
+        y='Response:O',
+        color = alt.Color('Response', legend=None) 
+    ).properties(
+        width=300,
+        height=350, title = {'text': ['How frequently do employees find meaningfulness at work?'],'subtitle': 'At country and designation level'}
+    )
 
     #vertical boxplot
-    data = vg_df[(vg_df['Country'] == xcol_vbarplot)]
-    vertical_boxplot = alt.Chart(data, title = "Workshop Preference (lower is more important)").mark_boxplot().encode(
+    # NEW Data wrangling for vertical bar graph
+    plot_data_2 = qwl_df[(qwl_df["Country of Residence "].isin([my_checklist_1]))]
+
+    workshop_topics = ['Stress Optimization',
+                        'Mindset Coaching',
+                        'Sleep Strategies',
+                        'Social Wellbeing',
+                        'Leadership and Teamwork',
+                        'Physical Health & Fitness',
+                        'Nutrition & Gut Health']
+
+    temp = plot_data_2.iloc[:,55:62]
+    temp = temp.set_axis(workshop_topics, axis=1)
+    temp['Country'] = plot_data_2.iloc[:,4]
+    temp = temp.set_index('Country')
+
+    vg_df = temp.melt(var_name = "Workshop_Topic", value_name = "Preference", ignore_index = False)
+    vg_df = vg_df.reset_index(level=0)
+
+
+    #data = vg_df[(vg_df['Country'].isin([my_checklist_1]))]
+    vertical_boxplot = alt.Chart(vg_df).mark_boxplot().encode(
         x = 'Workshop_Topic',
         y = 'Preference',
         color = alt.Color('Workshop_Topic', legend=None)
     ).properties(
-        width=400,
-        height=180
+        width=300,
+        height=350,title = {'text': ["Workshop Preference (lower is more important)"],'subtitle': 'At country level'}
     )
-    
-    col_name = '17. I experience MEANINGFULNESS at work ... (e.g. inspired, trusted, respected, purpose, seen and heard, acknowledged, fulfilled, growth, contribution to something greater, etc.) '
 
-    plot_data = qwl_df[col_name].value_counts().to_frame().reset_index()
-    plot_data = plot_data.rename(columns={col_name:'Count', 'index':'Response'})
-
-    chart = alt.Chart(plot_data).mark_bar().encode(
-                    x='Count:Q',
-                    y='Response:O',  
-                )
-
-    return lineplot.to_html(), vertical_boxplot.to_html(), chart.to_html()
+    return histogram.to_html(), chart.to_html(), vertical_boxplot.to_html()
 
 if __name__ == "__main__":
     app.run_server(debug=False)
